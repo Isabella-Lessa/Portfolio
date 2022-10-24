bfa4708ed9591ccb415bbd30531cb68610840995
Isabella <isabella.lessa27@gmail.com>
2022-10-24 00:30:37 -0300

    update projects

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,12 +1,87 @@
-import React from 'react';
-import * as S from './style.js';
+import React from "react";
+import * as S from "./style.js";
+import HP from "../../assets/hp-img.png";
+import DOG from "../../assets/adoption-img.png";
+import TRACKER from "../../assets/tracker-img.png";
+import TODO from "../../assets/todo-img.png";
+import Footer from './../../services/Footer/index';
 
 export default function Projects() {
   return (
-    <S.ProjectsConatiner>
-        <S.Container>
-            
-        </S.Container>
-    </S.ProjectsConatiner>
-  )
+    <S.ProjectsContainer>
+      <S.Container>
+      <S.Box>
+        <S.CardHP>
+          <S.ImgWorks src={HP} alt="harry potter project" />
+          <S.BoxBtns>
+            <S.BTN>
+              <S.A
+                href="https://github.com/Isabella-Lessa/Hogwarts-World"
+                target="_blank"
+              >
+                GitHub
+              </S.A>
+            </S.BTN>
+            <S.BTN>Online</S.BTN>
+          </S.BoxBtns>
+        </S.CardHP>
+        <S.CardDog>
+          <S.ImgWorks src={DOG} alt="adoption project" />
+          <S.BoxBtns>
+            <S.BTN>
+            <S.A
+              href="https://github.com/Isabella-Lessa/Adopt-me"
+              target="_blank"
+            >
+              GitHub
+            </S.A>
+            </S.BTN>
+            <S.BTN>Online</S.BTN>
+          </S.BoxBtns>
+        </S.CardDog>
+        <S.CardTracker>
+          <S.ImgWorks src={TRACKER} alt="alura tracker project" />
+          <S.BoxBtns>
+          <S.BTN>
+            <S.A
+              href="https://github.com/Isabella-Lessa/Alura-Tracker"
+              target="_blank"
+            >
+              GitHub
+            </S.A>
+            </S.BTN>
+            <S.BTN>
+              <S.A href="https://alura-tracker-six-rust.vercel.app/" target="_blank">
+                Online
+              </S.A>
+            </S.BTN>
+          </S.BoxBtns>
+        </S.CardTracker>
+        <S.CardToDo>
+          <S.ImgWorks src={TODO} alt="to do project" />
+          <S.BoxBtns>
+            <S.BTN>
+              <S.A href="https://github.com/Isabella-Lessa/ToDo" target="_blank">
+                GitHub
+              </S.A>
+            </S.BTN>
+            <S.BTN>
+              <S.A href="" target="_blank">
+                Online
+              </S.A>
+            </S.BTN>
+          </S.BoxBtns>
+        </S.CardToDo>
+        <S.CardOds>
+          <S.ImgWorks src="" alt="ods project" />
+          <S.BoxBtns>
+            <S.BTN>GitHub</S.BTN>
+            <S.BTN>Online</S.BTN>
+          </S.BoxBtns>
+        </S.CardOds>
+      </S.Box>
+      </S.Container>
+      <Footer/>
+    </S.ProjectsContainer>
+  );
 }
--- src/pages/Projects/style.js
+++ src/pages/Projects/style.js
@@ -1,12 +1,160 @@
 import styled from 'styled-components';
 
-export const ProjectsConatiner = styled.section`
+export const ProjectsContainer = styled.section`
     width: 100%;
     height: 100vh;
-    background-color: lightgray;
 `
 
 export const Container = styled.div`
+    height: 150%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Box = styled.div`
+    width: 90%;
+    display: flex;
+    flex-wrap: wrap;
+`
+
+export const CardHP = styled.div`
+    background-color: #0C2847;
+    width: 25em;
+    height: 20em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin: 18px;
+    transition: 1s all;
+    overflow: hidden;
+    border-radius: 10px;
+    box-shadow: -5px 2px 15px 6px rgba(0,0,0,0.20);
+    cursor: pointer;
+    &:hover{
+        transform: scale(1.1);
+    }
+`
+
+export const CardDog = styled.div`
+    background-color: #0C2847;
+    width: 25em;
+    height: 20em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin: 18px;
+    transition: 1s all;
+    overflow: hidden;
+    border-radius: 10px;
+    box-shadow: -5px 2px 15px 6px rgba(0,0,0,0.20);
+    cursor: pointer;
+    &:hover{
+        transform: scale(1.1);
+    }
+`
+
+export const CardTracker = styled.div`
+    background-color: #0C2847;
+    width: 25em;
+    height: 20em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin: 18px;
+    transition: 1s all;
+    overflow: hidden;
+    border-radius: 10px;
+    box-shadow: -5px 2px 15px 6px rgba(0,0,0,0.20);
+    cursor: pointer;
+    &:hover{
+        transform: scale(1.1);
+    }
+`
+
+export const CardToDo = styled.div`
+    background-color: #0C2847;
+    width: 25em;
+    height: 20em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin: 18px;
+    transition: 1s all;
+    overflow: hidden;
+    border-radius: 10px;
+    box-shadow: -5px 2px 15px 6px rgba(0,0,0,0.20);
+    cursor: pointer;
+    &:hover{
+        transform: scale(1.1);
+    }
+`
+
+export const CardOds = styled.div`
+    background-color: #0C2847;
+    width: 25em;
+    height: 20em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin: 18px;
+    transition: 1s all;
+    overflow: hidden;
+    border-radius: 10px;
+    box-shadow: -5px 2px 15px 6px rgba(0,0,0,0.20);
+    cursor: pointer;
+    &:hover{
+        transform: scale(1.1);
+    }
+`
+
+export const ImgWorks = styled.img`
+    width: 400px;
+    height: 230px;
+    object-fit: cover;
+`
+
+export const BoxBtns = styled.div`
     width: 100%;
-    height: 
+    height: 6.5em;
+    overflow: hidden;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    &:hover{
+        background-color: white
+    }
+`
+
+export const BTN = styled.button`
+    background-color: white;
+    width: 100px;
+    height: 2.5em;
+    font-size: 14px;
+    font-family:'Montserrat';
+    font-weight: 700;
+    border-radius: 10px;
+    border: none;
+    box-shadow: -3px 3px 10px 0px rgba(0,0,0,0.82);
+    transition: 0.5s all;
+    &:hover{
+        background-color: #0C2847;
+        color: white;
+        cursor: pointer
+    }
+`
+
+export const A = styled.a`
+    text-decoration: none;
+    color: black;
+
+    &:hover{
+        color: white;
+        cursor: pointer
+    }
 `
